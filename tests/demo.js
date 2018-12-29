603023a337e26f03ac154d101c7e1a8102136508
Harut Abgaryan <harutyun.abgaryan@gmail.com>
2018-12-29 15:13:09 +0400

    added return promise tests

--- tests/demo.js
+++ tests/demo.js
@@ -22,14 +22,20 @@ describe('test demo', ()=> {
     });
 
     context('shold test promise function', ()=>{
-        it('testing promise function', (done)=>{
-            demo.addPromise(1, 2).then((result)=> {
+        // it('testing promise function', (done)=>{
+        //     demo.addPromise(1, 2).then((result)=> {
+        //         expect(result).to.equal(3);
+        //         done();
+        //     }).catch((err)=> {
+        //         console.log('error promise ----')
+        //         done(err);
+        //     })
+        // });
+
+        it('testing return promise', ()=>{
+            return demo.addPromise(1, 2).then((result)=>{
                 expect(result).to.equal(3);
-                done();
-            }).catch((err)=> {
-                console.log('error promise ----')
-                done(err);
             })
-        })
+        });
     })
 })
