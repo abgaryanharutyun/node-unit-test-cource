0dcb824a37f66b146992a2d71f23dbdf8c639156
Harut Abgaryan <harutyun.abgaryan@gmail.com>
2018-12-29 19:41:41 +0400

    stub method

--- src/demo.js
+++ src/demo.js
@@ -10,6 +10,43 @@ exports.addCalback = function(a, b, callBack) {
 }
 
 exports.addPromise = function(a, b) {
-    // return Promise.reject(new Error('fails'));
+    // return Promise.reject(new Error('fake'));
     return Promise.resolve(a+b);
+}
+
+exports.foo = function() {
+    console.log('console.log was called');
+    console.warn('console.warn was called');
+
+    return;
+}
+
+exports.bar = async (fileName) => {
+    await exports.createFile(fileName);
+    let result = await callDb(fileName);
+
+    return result;
+}
+
+exports.createFile = (fileName)=>{
+    console.log('----in createFile');
+
+    //fake create file
+    return new Promise((resolve) => {
+        setTimeout(()=> {
+            console.log('fake file create');
+            return Promise.resolve('done')
+        }, 100)
+    })
+}
+
+
+function callDb(fileName) {
+    console.log('---- in callDb');
+    return new Promise((resolve) => {
+        setTimeout(()=> {
+            console.log('fake db called');
+            resolve('saved');
+        }, 100)
+    })
 }
--- tests/demo.js
+++ tests/demo.js
@@ -1,9 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
-var demo = require('../src/demo');
 
-
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const rewire = require('rewire');
+chai.use(sinonChai);
+var demo = rewire('../src/demo');
 describe('test demo', ()=> {
     context('add function', ()=> {
         it('shold add two numbers', () => {
@@ -37,5 +42,52 @@ describe('test demo', ()=> {
                 expect(result).to.equal(3);
             })
         });
+
+        it('shold tests promise awit/async', async ()=>{
+            const result = await demo.addPromise(1, 2);
+            expect(result).to.equal(3);
+        });
+
+        it('shold test promise with chai as promised', async ()=>{
+            await expect(demo.addPromise(1,2)).to.eventually.equal(3);
+        })
+    });
+
+    context('test doubles', ()=> {
+        it('Should spy on logs', () => {
+            let spy = sinon.spy(console, 'log');
+            demo.foo();
+
+            expect(spy.calledOnce).to.be.true;
+            expect(spy).to.have.been.calledOnce;
+            spy.restore();
+        });
+
+        it('Shold stub console.warn', ()=>{
+            let stub = sinon.stub(console, 'warn').callsFake(()=>{
+                console.log('message from stub');
+            });
+            
+            demo.foo();
+            expect(stub).to.have.been.calledOnce;
+            expect(stub).to.have.been.calledWith('console.warn was called');
+            stub.restore();
+        })
+    });
+
+    context('stub private function', ()=>{
+        it('should stub createFile', async ()=>{
+            let createStub = sinon.stub(demo, 'createFile').resolves('create_stub');
+            let callStub = sinon.stub().resolves('calledDb_stub');
+
+            demo.__set__('callDb', callStub);
+
+            let result = await demo.bar('test.txt');
+
+            expect(result).to.equal('calledDb_stub');
+            expect(createStub).to.have.been.calledOnce;
+            expect(createStub).to.have.been.calledWith('test.txt');
+            expect(callStub).to.have.been.calledOnce;
+        })
     })
 })
